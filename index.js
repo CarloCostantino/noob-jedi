4f5194168a5a4b8a7dfc0b51c624b7d0e2f5dc10
Carlo Costantino <costantinoart@gmail.com>
2019-09-04 08:30:46 -1000

    added data to index.js flexbox to css

---
+++ index.js
@@ -0,0 +1,76 @@
+const data = [{
+    q: 'What is the name of Luke Skywalkers father?',
+    a: 'Padmé Amidala',
+    b: 'Shmi Skywalker',
+    c: 'Aayla Secura',
+    d: 'Mon Mothma'
+  },
+  {
+    q: 'What is the name of the ship in the photo above?',
+    a: 'Slave 1',
+    b: 'Rebel Blockade Runner', 
+    c: 'Naboo N-1 Starfighter', 
+    d: 'R60 T-wing interceptor'
+  },
+  {
+    q: 'How did Han, Luke, Chewy and Leia escape the trash compacter on the Death Star?',
+    a: 'Chewy took a metal pipe and pried open the doors',
+    b: 'Luke found an hatch under the pile of trash',
+    c: 'C-3PO came to the door and opened it from the outside',
+    d: 'R2D2 accessed the trash compacter and shut it down remotely'
+  },
+  {
+    q: 'What is the name of the planet that Luke met Yoda?',
+    a: 'Coruscant',
+    b: 'Hoth',
+    c: 'Dagobah',
+    d: 'Naboo'
+  },
+  {
+    q: 'What did Darth Vader say to Luke after being accused of killing his father?',
+    a: 'No Luke, I am your father',
+    b: "Who's your daddy? I'm your daddy",
+    c: 'No, I am your father',
+    d: 'I am your father'
+  },
+  {
+    q: 'What color is Lukes light saber',
+    a: 'Green',
+    b: 'Purple',
+    c: 'White',
+    d: 'Blue'
+  },
+  {
+    q: 'What is the home planet of the Wookies?',
+    a: 'Endor',
+    b: 'Alderaan',
+    c: 'Kashyyyk',
+    d: 'Tatooine'
+  },
+  {
+    q: 'What did Luke see in the cave on Dagobah?',
+    a: 'All of his friends gathered around a table eating grilled Tauntaun',
+    b: 'Han Solo playing a flute with a flowers in his hair',
+    c: "Himself, under Darth Vader's mask",
+    d: 'A wise old Jedi Master named Yoda'
+  },
+  {
+    q: 'What did Luke need to pick up at Tosche Station on Tatooine?',
+    a: 'Some milk for breakfast',
+    b: 'A part for C-3PO',
+    c: 'Power converters',
+    d: 'His friend Bob'
+  },
+  {
+    q: 'Who knew it was a trap?',
+    a: 'Han Solo',
+    b: 'Jabba the Hutt',
+    c: 'Obi-Wan Kenobi',
+    d: 'Admiral Ackbar'
+  }]
+  
+const correct = `Nice job kid, don't get cocky!`
+const incorrect = `Wrong, it was actually "${rightAnswer}"`
+
+
+
